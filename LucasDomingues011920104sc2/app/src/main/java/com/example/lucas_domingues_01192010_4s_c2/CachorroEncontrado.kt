3d1d99ba6796d956e7ae6b3f80e0a681eec48bd9
lucasdominguesbandtec <lucas.domingues@bandtec.com.br>
2021-04-14 22:11:18 -0300

    criação da tela cachorro encontrado

--- LucasDomingues011920104sc2/app/src/main/java/com/example/lucas_domingues_01192010_4s_c2/CachorroEncontrado.kt
+++ LucasDomingues011920104sc2/app/src/main/java/com/example/lucas_domingues_01192010_4s_c2/CachorroEncontrado.kt
@@ -1,4 +1,32 @@
 package com.example.lucas_domingues_01192010_4s_c2
 
-class CachorroEncontrado {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CachorroEncontrado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cachorro_encontrado)
+
+        val cachorroEncontrado = Intent(this, CachorroEncontrado::class.java)
+
+        val cachorro1: Cachorro ?= null
+        val cachorro2: Cachorro ?= null
+
+        if(cachorro1 == null) {
+            cachorroEncontrado.putExtra("racaCachorro1", getString(R.string.nao_encontrado))
+        } else {
+            cachorroEncontrado.putExtra("racaCachorro1", cachorro1.raca)
+        }
+
+        if(cachorro2 == null) {
+            cachorroEncontrado.putExtra("racaCachorro2", getString(R.string.nao_encontrado))
+        } else {
+            cachorroEncontrado.putExtra("racaCachorro2", cachorro2.raca)
+        }
+
+        startActivity(cachorroEncontrado)
+    }
+
 }
